0c6798f935c6d3e3caaa12c4d15bbf456becc233
Gildas <gvinsot@hotmail.com>
2020-02-11 21:21:42 +0000

    fix init script

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get update
-sudo apt-get install -y flask_cors python3-venv
+sudo apt-get install -y python3-venv python3-pip
 sudo python3 -m venv venv
 source venv/bin/activate
-
+sudo pip3 install -r requirements.txt
 
